aa6b8dd6f6c838a60b806bbd0bffd284400d37c5
Stefan Jovanovic <stefanpo92@gmail.com>
2014-03-16 15:20:17 +0100

    Again...

--- src/application_logic/SetMyShipsManager.java
+++ src/application_logic/SetMyShipsManager.java
@@ -1,7 +1,6 @@
 package application_logic;
 
 import java.awt.Dimension;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
